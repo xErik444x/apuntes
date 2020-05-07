544714dda0b126024803b2bbfd4c3237d757ab2f
xErik444x <erikschwerdt18@gmail.com>
2020-05-07 13:55:27 -0300

    codes/split_Propio:

--- codes/split_Propio.py
+++ codes/split_Propio.py
@@ -1,17 +1,20 @@
 def misplit(strng):
     array = []
     palabra = ""
-    for x in range(len(strng)):
-        if strng[x] == " " :
-            array.append(palabra)
-            palabra = ""
+    for x in range(len(strng)): # Recorro todo el String
+        if strng[x].isspace() : # Si la posicion actual tiene espacio
+            if len(palabra) > 0: # Si la palabra formada es mayor que 0
+                array.append(palabra) # agregar la palabra al array
+                palabra = "" # reiniciar valor de la palabra
         else:
-            palabra += strng[x]
+            palabra += strng[x] # Si no tiene espacio, añadir el caracter para formar otra palabra
+        if x == len(strng)-1 and len(palabra)> 0:
+            array.append(palabra)
     return(array)
 
-print(len(misplit("Ser o no ser, esa es la pregunta")))
+print(misplit("Ser o no ser, esa es la pregunta"))
 print(misplit("Ser o no ser,esa es la pregunta"))
+print(misplit(" espacio sin espacio con espacio del espacio "))
 print(misplit("   "))
 print(misplit(" abc "))
 print(misplit(""))
-print(("Ser o no ser, esa es la pregunta").split(" "))
